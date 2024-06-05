91208e2523b1a569ef368933b58f619d0252883d
Adam Howard <ahoward0920@gmail.com>
2024-06-04 22:29:31 -0400

    Fixing URL

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1', 
     description='Configuration definition, parsing, and validation, in the style of argparse.',
     long_description=open("README.rst", 'r').read(),
-    url="github.com/Elemnir/comfyparse",
+    url="https://github.com/Elemnir/comfyparse",
     author='Adam Howard', 
     author_email='ahoward0920@gmail.com', 
     packages=['comfyparse'], 
